ab4e7eb5450914d8b9c7e33785af95f7245b815f
Bilal Younas <bilalyounas@Bilals-MacBook-Air.local>
2023-03-26 17:58:19 +0500

    delete table before deleting database

--- standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/TestPartitionManagement.java
+++ standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/TestPartitionManagement.java
@@ -91,14 +91,17 @@ public class TestPartitionManagement {
           // First drop any databases in catalog
           List<String> databases = client.getAllDatabases(catName);
           for (String db : databases) {
-            client.dropDatabase(catName, db, false, false, true);
+            client.dropDatabase(catName, db, true, false, true);
           }
           client.dropCatalog(catName);
         } else {
           List<String> databases = client.getAllDatabases(catName);
           for (String db : databases) {
             if (!db.equalsIgnoreCase(Warehouse.DEFAULT_DATABASE_NAME)) {
-              client.dropDatabase(catName, db, false,false, true);
+              for (String table : client.listTableNamesByFilter(db, "", (short) -1)) {
+                client.dropTable(db, table, true, true);
+              }
+              client.dropDatabase(catName, db, true ,false, true);
             }
           }
         }
