4d67273318423792059304eaa485831a36ba0979
Bilal Younas <bilalyounas@Bilals-MacBook-Air.local>
2023-03-27 02:10:36 +0500

    flaky-fix base code

--- standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/TestPartitionManagement.java
+++ standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/TestPartitionManagement.java
@@ -98,9 +98,6 @@ public class TestPartitionManagement {
           List<String> databases = client.getAllDatabases(catName);
           for (String db : databases) {
             if (!db.equalsIgnoreCase(Warehouse.DEFAULT_DATABASE_NAME)) {
-              for (String table : client.getAllTables(db)) {
-                client.dropTable(db, table, true, true);
-              }
               client.dropDatabase(catName, db, true ,false, true);
             }
           }
